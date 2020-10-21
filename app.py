fd2386b9176644c983ec75615ec5f5e732b738f1
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-10-21 11:00:58 -0700

    updated to show user bio and follower / following users bios

--- app.py
+++ app.py
@@ -208,7 +208,9 @@ def profile():
     """Update profile for current user."""
 
     # IMPLEMENT THIS
-
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
 
 @app.route('/users/delete', methods=["POST"])
 def delete_user():
